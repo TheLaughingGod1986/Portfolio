fab6c9241b27d3bd2697400fda1f8d36c06ddcdd
TheLaughingGod1986 <benoats@gmail.com>
2016-01-13 20:16:15 +0000

    added new directory and file

---
+++ welcome/form.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html lang="en">
+<meta charset="utf-8">
+<title>Form Example</title>
+
+<body>
+<form action="" method="post">
+    <div> <p>text</p>
+    </div>
+
+</form>
+</body>
+
+</html>
